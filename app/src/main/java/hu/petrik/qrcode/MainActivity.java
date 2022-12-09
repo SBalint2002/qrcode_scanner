d2c25f65131742e72471eca43b1e0352f9e6860d
Sárándi <sarandi.balint@gmail.com>
2022-12-09 09:15:18 +0100

    scan qr code with camera

--- app/src/main/java/hu/petrik/qrcode/MainActivity.java
+++ app/src/main/java/hu/petrik/qrcode/MainActivity.java
@@ -1,8 +1,11 @@
 package hu.petrik.qrcode;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -15,6 +18,8 @@ import com.google.zxing.BarcodeFormat;
 import com.google.zxing.MultiFormatWriter;
 import com.google.zxing.WriterException;
 import com.google.zxing.common.BitMatrix;
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 import com.journeyapps.barcodescanner.BarcodeEncoder;
 
 public class MainActivity extends AppCompatActivity {
@@ -50,6 +55,36 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+        buttonScan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                IntentIntegrator intentIntegrator =
+                        new IntentIntegrator(MainActivity.this);
+                intentIntegrator.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE);
+                intentIntegrator.setCameraId(0);
+                intentIntegrator.setBeepEnabled(false);
+                intentIntegrator.setBarcodeImageEnabled(false);
+                intentIntegrator.initiateScan();
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if (result != null){
+            if(result.getContents() == null) {
+                Toast.makeText(this, "Visszaléptél a scann-ből", Toast.LENGTH_SHORT).show();
+            }else{
+                textViewQRResult.setText(result.getContents());
+                Uri uri = Uri.parse(result.getContents());
+
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(intent);
+            }
+        }else{
+            super.onActivityResult(requestCode, resultCode, data);
+        }
     }
 
     private void init(){
